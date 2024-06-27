9c7ba22812b5cda19a4a2d06da39f92fc11e7f53
nickthorton <nishandevkarnd@gmail.com>
2024-06-27 13:22:08 +0800

    StrictMode Demonstration
    - Changes the message component to demonstrate
    that StrictMode renders a component twice to check for impure functions

--- nishan-mosh-follow-along/src/App.tsx
+++ nishan-mosh-follow-along/src/App.tsx
@@ -1,12 +1,13 @@
 // This works because TypeScript looks for index.ts
 // if it doesnt find the ./components/ListGroup/ListGroup
-import LikeButton from "./components/LikeButton/LikeButton";
-import AsyncDemo from "./components/AsyncDemo/AsyncDemo";
+import Message from "./Message";
+
 function App() {
 	return (
 		<div>
-			<LikeButton></LikeButton>
-			<AsyncDemo></AsyncDemo>
+			<Message></Message>
+			<Message></Message>
+			<Message></Message>
 		</div>
 	);
 }
--- nishan-mosh-follow-along/src/Message.tsx
+++ nishan-mosh-follow-along/src/Message.tsx
@@ -1,10 +1,8 @@
 function Message() {
-	const messageText = "messass";
-	if (messageText) return <h1>This is a {messageText} component heading</h1>;
-	return (
-		<h1>
-			This is a message component without dynamic changing of variable
-		</h1>
-	);
+	// When count is part of the component,
+	// it is a pure function
+	let count = 0;
+	count++;
+	return <h1> Message {count}</h1>;
 }
 export default Message;
