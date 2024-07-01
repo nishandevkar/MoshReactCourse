7043d09cb43e47755cd1c029a4e86e7080a4fa72
nickthorton <nishandevkarnd@gmail.com>
2024-07-01 13:03:09 +0800

    Uses React-Hook-Form for form validation
    - uses useForm hook, validates data and shows the errors
    - standard HTML validaton can be used with the register method

--- nishan-mosh-follow-along/src/components/BasicForm/BasicForm.tsx
+++ nishan-mosh-follow-along/src/components/BasicForm/BasicForm.tsx
@@ -1,46 +1,62 @@
-import { FormEvent, useState } from "react";
 import "./BasicForm.css";
+import { useForm, FieldValues } from "react-hook-form";
+interface FormData {
+	name: string;
+	age: number;
+}
 const BasicForm = () => {
-	const [person, setPerson] = useState({
-		name: "",
-		age: "",
-	});
-	const handleSubmit = (event: FormEvent) => {
-		event.preventDefault();
-		console.log(person.name, person.age);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormData>();
+
+	const onSubmit = (data: FieldValues) => {
+		console.log(data);
 	};
 	return (
-		<form onSubmit={handleSubmit}>
+		<form onSubmit={handleSubmit(onSubmit)}>
 			<div className="mb-3">
 				<label htmlFor="name" className="form-label">
 					Name
 				</label>
 				<input
-					onChange={(event) =>
-						setPerson({ ...person, name: event.target.value })
-					}
-					value={person.name}
+					{...register("name", { required: true, minLength: 3 })}
 					id="name"
 					type="text"
 					className="form-control"
 				/>
+				{errors.name?.type === "required" && (
+					<p className="text-danger">The name is required</p>
+				)}
+				{errors.name?.type === "minLength" && (
+					<p className="text-danger">
+						The name should be 3 characters long
+					</p>
+				)}
 			</div>
 			<div className="mb-3">
 				<label htmlFor="age" className="form-label">
 					Age
 				</label>
 				<input
-					onChange={(event) =>
-						setPerson({
-							...person,
-							age: event.target.value,
-						})
-					}
-					value={person.age}
+					{...register("age", { required: true, min: 0, max: 117 })}
 					id="age"
 					type="number"
 					className="form-control"
 				/>
+				{errors.age?.type === "required" && (
+					<p className="text-danger">Age is required</p>
+				)}
+				{errors.age?.type === "min" && (
+					<p className="text-danger">Age cannot be negative</p>
+				)}
+				{errors.age?.type === "max" && (
+					<p className="text-warning">
+						You are older than the current oldest person in the
+						world, Congratulations
+					</p>
+				)}
 			</div>
 			<button className="btn btn-primary" type="submit">
 				Submit{" "}
