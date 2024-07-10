9957a6bc99c9390507b4e5c53d12af9daee22898
nickthorton <nishandevkarnd@gmail.com>
2024-07-10 12:20:09 +0800

    Shows catch error example in axios
    -renders the error message using the AxiosError

--- nishan-mosh-follow-along/src/components/UseEffectHook/FetchAPIExample.tsx
+++ nishan-mosh-follow-along/src/components/UseEffectHook/FetchAPIExample.tsx
@@ -11,21 +11,26 @@ interface Comments {
 }
 const FetchAPIExample = () => {
 	const [comments, setComments] = useState<Comments[]>([]);
+	const [error, setError] = useState("");
 	useEffect(() => {
 		axios
 			.get<Array<Comments>>(
-				"https://jsonplaceholder.typicode.com/comments"
+				"https://jsonplaceholder.typicode.com/xcomments"
 			)
-			.then((response) => setComments(response.data));
+			.then((response) => setComments(response.data))
+			.catch((err) => setError(err.message));
 	}, []);
 	return (
-		<div>
-			<ul>
-				{comments.map((data) => (
-					<li key={data.name}>{data.name}</li>
-				))}
-			</ul>
-		</div>
+		<>
+			<p>{error}</p>
+			<div>
+				<ul>
+					{comments.map((data) => (
+						<li key={data.name}>{data.name}</li>
+					))}
+				</ul>
+			</div>
+		</>
 	);
 };
 
